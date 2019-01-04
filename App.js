62e0aac86ce15c640d3604f640409b871c7ce701
Kenzaburo Takagi <ken.takagi216@gmail.com>
2019-01-04 10:38:21 +0900

    4-1-3

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Platform, ScrollView } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    this.state = {
+      todo: [
+        { index: 1, title: "原稿を書く", done: false },
+        { index: 2, title: "犬の散歩をする", done: false },
+      ],
+      currentIndex: 2
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,6 +23,10 @@ export default class App extends React.Component {
           <Text>Fileterがここに配置されます</Text>
         </View>
         <ScrollView style={styles.todolist}>
+          <FlatList data={this.state.todo}
+            renderItem={({ item }) => <Text>{item.title}</Text>}
+            keyExtractor={(item, index) => "todo_" + item.index}
+          />
           <Text>TodoListがここに配置されます</Text>
         </ScrollView>
         <View style={styles.input}>
