2b48af3c21a45c674cf9fc6398d5b5b1eb3af724
Kenzaburo Takagi <ken.takagi216@gmail.com>
2019-01-04 21:31:44 +0900

    4-1-4

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Platform, ScrollView } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, StatusBar, Platform, ScrollView, FlatList, TextInput, Button, KeyboardAvoidingView } from 'react-native';
+// import { FlatList } from 'react-native-gesture-handler';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
@@ -8,17 +8,30 @@ export default class App extends React.Component {
 
   constructor(props) {
     this.state = {
-      todo: [
-        { index: 1, title: "原稿を書く", done: false },
-        { index: 2, title: "犬の散歩をする", done: false },
-      ],
-      currentIndex: 2
+      todo: [],
+      currentIndex: 0,
+      inputText: "",
     }
+  }
+
+  onAddItem = () => {
+    const title = this.state.inputText
+    if (title == "") {
+      return
+    }
+    const index = this.state.currentIndex + 1
+    const newTodo = { index: index, title: title, done: false }
+    const todo = [...this.state.todo, newTodo]
+    this.setState({
+      todo: todo,
+      currentIndex: index,
+      inputText: ""
+    })
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
         <View style={styles.filter}>
           <Text>Fileterがここに配置されます</Text>
         </View>
@@ -30,10 +43,19 @@ export default class App extends React.Component {
           <Text>TodoListがここに配置されます</Text>
         </ScrollView>
         <View style={styles.input}>
-          <Text>テキスト入力がここに配置されます</Text>
+          <TextInput
+            onChangeText={(text) => this.setState({ inputText: text })}
+            value={this.state.inputText}
+            style={styles.inputText}
+          />
+          <Button
+            onPress={this.onAddItem}
+            title="Add"
+            color="#841584"
+            style={styles.inputButton}
+          />
         </View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -54,5 +76,12 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 30,
+    flexDirection: 'row',
   },
+  inputText: {
+    flex: 1,
+  },
+  inputButton: {
+    width: 100,
+  }
 });
